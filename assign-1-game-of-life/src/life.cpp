a7084989f484b33ba3ba517217e19b186270c168
exAClior <yushengzhao2020@outlook.com>
2022-08-23 16:20:28 +0800

    start writing load config function

--- assign-1-game-of-life/src/life.cpp
+++ assign-1-game-of-life/src/life.cpp
@@ -4,32 +4,27 @@
  * Implements the Game of Life.
  */
 
-#include <iostream>  // for cout
+#include <fstream>  // open file
+#include <iostream> // for cout
+#include <string>   // play with string
 using namespace std;
 
 #include "console.h" // required of all files that contain the main function
-#include "simpio.h"  // for getLine
 #include "gevents.h" // for mouse event detection
+#include "simpio.h"  // for getLine
 #include "strlib.h"
 
-#include "life-constants.h"  // for kMaxAge
-#include "life-graphics.h"   // for class LifeDisplay
+#include "life-constants.h" // for kMaxAge
+#include "life-graphics.h"  // for class LifeDisplay
 
 /**
  * Function: welcome
  * -----------------
  * Introduces the user to the Game of Life and its rules.
  */
-static void welcome() {
-    cout << "Welcome to the game of Life, a simulation of the lifecycle of a bacteria colony." << endl;
-    cout << "Cells live and die by the following rules:" << endl << endl;
-    cout << "\tA cell with 1 or fewer neighbors dies of loneliness" << endl;
-    cout << "\tLocations with 2 neighbors remain stable" << endl;
-    cout << "\tLocations with 3 neighbors will spontaneously create life" << endl;
-    cout << "\tLocations with 4 or more neighbors die of overcrowding" << endl << endl;
-    cout << "In the animation, new cells are dark and fade to gray as they age." << endl << endl;
-    getLine("Hit [enter] to continue....   ");
-}
+static void welcome();
+
+static void getStartConfig(int &row, int &column);
 
 /**
  * Function: main
@@ -37,8 +32,45 @@ static void welcome() {
  * Provides the entry point of the entire program.
  */
 int main() {
-    LifeDisplay display;
-    display.setTitle("Game of Life");
-    welcome();
-    return 0;
+  LifeDisplay display;
+  // prepare row and column
+  int row, column;
+  display.setTitle("Game of Life");
+  welcome();
+  getStartConfig(row, column);
+  getLine("Give me some time to look at this");
+  return 0;
+}
+
+static void welcome() {
+  cout << "Welcome to the game of Life, a simulation of the lifecycle of a "
+          "bacteria colony."
+       << endl;
+  cout << "Cells live and die by the following rules:" << endl << endl;
+  cout << "\tA cell with 1 or fewer neighbors dies of loneliness" << endl;
+  cout << "\tLocations with 2 neighbors remain stable" << endl;
+  cout << "\tLocations with 3 neighbors will spontaneously create life" << endl;
+  cout << "\tLocations with 4 or more neighbors die of overcrowding" << endl
+       << endl;
+  cout << "In the animation, new cells are dark and fade to gray as they age."
+       << endl
+       << endl;
+  getLine("Hit [enter] to continue....   ");
+}
+
+static void getStartConfig(int &row, int &column) {
+  string filename =
+      getLine("Provide a file name that you wish to load config from");
+  ifstream my_file;
+  my_file.open("files/" + filename);
+
+  if (!my_file) {
+    cout << filename << " does not exist, will load random config"
+         << endl;
+  } else {
+    string line;
+    while (getline(my_file, line)) {
+      cout << line << endl;
+    }
+  }
 }
