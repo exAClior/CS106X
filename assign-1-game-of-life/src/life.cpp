c304226c700f01d3e330797e7261146bc4278bf0
exAClior <yushengzhao2020@outlook.com>
2022-08-25 17:24:48 +0800

    got everything moving

--- assign-1-game-of-life/src/life.cpp
+++ assign-1-game-of-life/src/life.cpp
@@ -7,6 +7,10 @@
 #include <fstream>  // open file
 #include <iostream> // for cout
 #include <string>   // play with string
+// enable sleeping
+#include <chrono> // std::chrono::seconds
+#include <thread> // std::this_thread::sleep_for
+
 using namespace std;
 
 #include "console.h" // required of all files that contain the main function
@@ -26,7 +30,11 @@ static void welcome();
 
 static void getStartConfig(int &row, int &column, vector<Grid<int>> &worlds);
 
-static void drawOnDisp(Grid<int> & world, LifeDisplay & display);
+static void drawOnDisp(Grid<int> &world, LifeDisplay &display);
+
+static void evolveWorld(vector<Grid<int>> &worlds, int &curIdx);
+
+static int countNeighbours(Grid<int> &world, int &row, int &col);
 /**
  * Function: main
  * --------------
@@ -44,20 +52,71 @@ int main() {
   welcome();
   getStartConfig(row, column, worlds);
   display.setDimensions(row, column);
-  drawOnDisp(worlds[curIdx],display);
-  display.repaint();
-  getLine("Give me some time to look at this");
+  drawOnDisp(worlds[curIdx], display);
+  // getLine("Give me some time to look at this");
+  for (int jj = 0; jj < 1000000; jj++) {
+
+    evolveWorld(worlds, curIdx);
+    drawOnDisp(worlds[curIdx], display);
+    // later change this to implement varying time
+    this_thread::sleep_for(chrono::seconds(1));
+  }
   return 0;
 }
 
-static void drawOnDisp(Grid<int> & world, LifeDisplay & display){
+static int countNeighbours(Grid<int> &world, int &row, int &col) {
+  int row_arr[3] = {row - 1, row, row + 1};
+  int col_arr[3] = {col - 1, col, col + 1};
   int rows = world.numRows();
   int cols = world.numCols();
-  for (int ii = 0 ; ii < rows; ii ++){
-    for (int jj = 0; jj < cols; jj ++){
-      display.drawCellAt(ii,jj,world[ii][jj]);
+  int neighbours = 0;
+  for (int ii : row_arr) {
+    for (int jj : col_arr) {
+      if (!(ii == row && jj == col)) {
+        if ((ii >= 0 && ii < rows) && (jj >= 0 && jj < cols)) {
+          if (world[ii][jj] > 0) {
+            neighbours += 1;
+          }
+        }
+      }
     }
   }
+  return neighbours;
+}
+static void evolveWorld(vector<Grid<int>> &worlds, int &curIdx) {
+  int nxtIdx = (curIdx + 1) % 2;
+  int rows = worlds[curIdx].numRows();
+  int cols = worlds[curIdx].numCols();
+  int neighbours = 0;
+  for (int ii = 0; ii < rows; ii++) {
+    for (int jj = 0; jj < cols; jj++) {
+      neighbours = countNeighbours(worlds[curIdx], ii, jj);
+      if (neighbours <= 1) {
+        worlds[nxtIdx][ii][jj] = 0;
+      } else if (neighbours == 2) {
+        worlds[nxtIdx][ii][jj] = worlds[curIdx][ii][jj] + 1;
+        if (worlds[nxtIdx][ii][jj] > kMaxAge) {
+          worlds[nxtIdx][ii][jj] = 0;
+        }
+      } else if (neighbours == 3) {
+        // if there were life previously what do I do?
+        worlds[nxtIdx][ii][jj] = randomInteger(1, kMaxAge);
+      } else {
+        worlds[nxtIdx][ii][jj] = 0;
+      }
+    }
+  }
+  curIdx = nxtIdx;
+}
+static void drawOnDisp(Grid<int> &world, LifeDisplay &display) {
+  int rows = world.numRows();
+  int cols = world.numCols();
+  for (int ii = 0; ii < rows; ii++) {
+    for (int jj = 0; jj < cols; jj++) {
+      display.drawCellAt(ii, jj, world[ii][jj]);
+    }
+  }
+  display.repaint();
 }
 
 static void welcome() {
@@ -89,8 +148,8 @@ static void getStartConfig(int &row, int &column, vector<Grid<int>> &worlds) {
     row = randomInteger(40, 60);
     column = randomInteger(40, 60);
 
-    curWorld.resize(row,column);
-    nextWorld.resize(row,column);
+    curWorld.resize(row, column);
+    nextWorld.resize(row, column);
 
     for (int ii = 0; ii < row; ii++) {
       for (int jj = 0; jj < column; jj++) {
