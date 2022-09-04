87626b2b4f31a1f72100600d36b02e6939186488
exAClior <yushengzhao2020@outlook.com>
2022-09-04 11:15:20 +0800

    update sentence generator

--- assign-2-adts/random-sentence-generator/src/random-sentence-generator.cpp
+++ assign-2-adts/random-sentence-generator/src/random-sentence-generator.cpp
@@ -6,45 +6,68 @@
  * sentences from them.
  */
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 #include "console.h"
-#include "simpio.h"   // for getLine
-#include "strlib.h"   // for toLowerCase, trim
+#include "simpio.h" // for getLine
+#include "strlib.h" // for toLowerCase, trim
 
 static const string kGrammarsDirectory = "grammars/";
 static const string kGrammarFileExtension = ".g";
 static string getNormalizedFilename(string filename) {
-    string normalizedFileName = kGrammarsDirectory + filename;
-    if (!endsWith(normalizedFileName, kGrammarFileExtension))
-        normalizedFileName += kGrammarFileExtension;
-    return normalizedFileName;
+  string normalizedFileName = kGrammarsDirectory + filename;
+  if (!endsWith(normalizedFileName, kGrammarFileExtension))
+    normalizedFileName += kGrammarFileExtension;
+  return normalizedFileName;
 }
 
 static bool isValidGrammarFilename(string filename) {
-    string normalizedFileName = getNormalizedFilename(filename);
-    ifstream infile(normalizedFileName.c_str());
-    return !infile.fail();
+  string normalizedFileName = getNormalizedFilename(filename);
+  ifstream infile(normalizedFileName.c_str());
+  return !infile.fail();
 }
 
 static string getFileName() {
-    while (true) {
-        string filename = trim(getLine("Name of grammar file? [<return> to quit]: "));
-        if (filename.empty() || isValidGrammarFilename(filename)) return filename;
-        cout << "Failed to open the grammar file named \"" << filename << "\". Please try again...." << endl;
-    }
+  while (true) {
+    string filename =
+        trim(getLine("Name of grammar file? [<return> to quit]: "));
+    if (filename.empty() || isValidGrammarFilename(filename))
+      return filename;
+    cout << "Failed to open the grammar file named \"" << filename
+         << "\". Please try again...." << endl;
+  }
 }
 
-int main() {
-    while (true) {
-        string filename = getFileName();
-        if (filename.empty()) break;
-        cout << "Here's where you read in the \"" << filename << "\" grammar "
-             << "and generate three random sentences." << endl;
+static void loadFile(string filename) {
+  ifstream my_file;
+  string my_line;
+  my_file.open(getNormalizedFilename(filename));
+  if (my_file.is_open()) {
+    while (my_file) {
+      getline(my_file, my_line);
+      // make a queue/or linked list? for first line
+      // make a map of <string> and list of lines for the rest
+      // pop continuously of first line
+      // each time non <>, pinrt
+      //  <> expand and push line back into queue? this is problematic,
+      cout << my_line << endl;
     }
-    
-    cout << "Thanks for playing!" << endl;
-    return 0;
+  }
+}
+int main() {
+  while (true) {
+    string filename = getFileName();
+    if (filename.empty())
+      break;
+    cout << "Here's where you read in the \"" << filename << "\" grammar "
+         << "and generate three random sentences." << endl;
+    loadFile(filename);
+    // for (int i = 0; i < 3; i++) {
+    // }
+  }
+
+  cout << "Thanks for playing!" << endl;
+  return 0;
 }
